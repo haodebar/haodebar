c7bb6695df470a3172b37d19967e9a48ad2ec349
xuzhichao <1352425876@qq.com>
2019-12-05 17:26:48 +0800

    基础架子搭建

--- manager/src/main/java/com/chaoyue/manager/usermanage/controller/UserController.java
+++ manager/src/main/java/com/chaoyue/manager/usermanage/controller/UserController.java
@@ -5,7 +5,7 @@ import com.chaoyue.manager.usermanage.model.UserModel;
 import com.chaoyue.manager.usermanage.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -20,10 +20,9 @@ public class UserController implements UserControllerApi {
     @Autowired
     UserService userService;
     @Override
-    @GetMapping("saveData")
+    @PostMapping ("/saveData")
     public void SaveData() {
         log.info("xzc");
-        System.out.println("我进入方法");
         UserModel model = new UserModel();
         model.setVersion("版本");
         userService.save(model);
