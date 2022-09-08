162f4d7dcf4c16fd9444ebf8691428de2c770b56
xuzhichao <1352425876@qq.com>
2022-09-08 22:32:44 +0800

    项目架构调整

--- haodebar-main/src/main/java/com/chaoyue/haodebar/ManagerApplication.java
+++ haodebar-main/src/main/java/com/chaoyue/haodebar/ManagerApplication.java
@@ -3,11 +3,13 @@ package com.chaoyue.haodebar;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan(basePackages = {"com.chaoyue.**.**.dao"})
 @ComponentScan("com.chaoyue")
+@EnableDiscoveryClient
 public class ManagerApplication {
 
     public static void main(String[] args) {
