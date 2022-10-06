86219e2b452ff498b98481ab010152ce1a7f56b8
xuzhichao <1352425876@qq.com>
2022-10-06 13:05:56 +0800

    项目架构调整

---
+++ haodebar-main/src/main/java/com/chaoyue/haodebar/usermanage/domain/UserReqDto.java
@@ -0,0 +1,12 @@
+package com.chaoyue.haodebar.usermanage.domain;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author: xuzhichao8
+ * @date: 2022/9/9 11:39
+ * @version: version 1.0
+ * @dec: 请求参数
+ */
+public class UserReqDto {
+}
