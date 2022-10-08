bc7322b5ffe8228c895982c2f17f1bffbae5bfeb
xuzhichao <1352425876@qq.com>
2022-10-08 17:31:27 +0800

    项目架构调整

--- haodebar-main/src/main/java/com/chaoyue/haodebar/usermanage/api/UserControllerApi.java
+++ haodebar-main/src/main/java/com/chaoyue/haodebar/usermanage/api/UserControllerApi.java
@@ -12,5 +12,5 @@ import io.swagger.annotations.ApiOperation;
 @Api(tags = "用户管理")
 public interface UserControllerApi {
     @ApiOperation(value = "用户新增",notes = "用新增接口")
-    UserModel SaveData();
+    UserModel saveData();
 }
--- haodebar-main/src/main/java/com/chaoyue/haodebar/usermanage/controller/UserController.java
+++ haodebar-main/src/main/java/com/chaoyue/haodebar/usermanage/controller/UserController.java
@@ -9,30 +9,33 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 /**
- * Date:2019/12/4
- * Create by:xuzhichao
+ * @author: xuzhichao
+ * @date: 2019/12/4
  * Describe:
  */
 @Slf4j
 @RestController("/userManager")
 public class UserController implements UserControllerApi {
 
-    @Autowired
-    UserService userService;
-    @Autowired
+    @Resource
+    private UserService userService;
+    @Resource
     private RedisTemplate redisTemplate;
+
     @Override
     @PostMapping ("/saveData")
-    public UserModel SaveData() {
+    public UserModel saveData() {
         log.info("xzc");
         UserModel model = new UserModel();
         model.setVersion("版本");
         model.setUserName("许智超");
         model.setEmail("xzc@qq.com");
         model.setPhone("18810440946");
-        //userService.save(model);
         redisTemplate.opsForValue().set("xzctset",1);
+        userService.save(model);
         return model;
     }
 }
